ba1df38e8c7af451404601df96a9b654b8aa9279
lizhijian <907257608@qq.com>
2018-02-04 17:27:16 +0800

    add auto generator code

---
+++ src/main/java/com/wuxin/controller/CrudController.java
@@ -0,0 +1,14 @@
+package com.wuxin.controller;
+
+import org.springframework.stereotype.Controller;
+
+import java.io.Externalizable;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class CrudController {
+
+
+
+}
