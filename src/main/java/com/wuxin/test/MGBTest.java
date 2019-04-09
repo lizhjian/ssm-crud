3df280e88276c0eb7499d96bf9c7b63fbe307305
lizj2 <907257608@qq.com>
2019-04-09 16:32:13 +0800

    新建实体

--- src/main/java/com/wuxin/test/MGBTest.java
+++ src/main/java/com/wuxin/test/MGBTest.java
@@ -13,7 +13,7 @@ public class MGBTest {
     public static  void main(String[] args) throws Exception{
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-        File configFile = new File("mbg.xml");
+        File configFile = new File("generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
